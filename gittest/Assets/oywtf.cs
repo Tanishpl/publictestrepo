f3aec1c9e211fa82d15d68abfb375fb0850171ef
Tanishpl <tanishpatel2007@gmail.com>
2024-02-24 13:31:14 +0000

    gittesttestetst
    idk what this i s

---
+++ gittest/Assets/oywtf.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oywtf : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Cube_Cube;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
